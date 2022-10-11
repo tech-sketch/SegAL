1292ab658c4dc830174e5cfd64dc85350cd84102
BrambleXu <liangxu006@gmail.com>
2022-10-11 18:21:54 +0900

    feat: update plot function and add save, load function

--- segal/utils.py
+++ segal/utils.py
@@ -1,12 +1,20 @@
+import json
+from typing import Any, List
+
 import matplotlib.pyplot as plt
 
 
 def plot_loss(
-    loss_train_logs, loss_val_logs, loss_test_logs, save_path="./output/loss.png"
+    full_result, random_result, strategy_result, save_path="./output/loss.png"
 ):
-    plt.plot(loss_train_logs, label="train", marker="o")
-    plt.plot(loss_val_logs, label="val", marker="o")
-    plt.plot(loss_test_logs, label="val", marker="o")
+
+    random_loss = [log["dice_loss"] for log in random_result]
+    strategy_loss = [log["dice_loss"] for log in strategy_result]
+    full_loss = [full_result["dice_loss"]] * len(random_loss)
+
+    plt.plot(full_loss, label="Supervised", linestyle="dashed")
+    plt.plot(random_loss, label="Random", marker="o")
+    plt.plot(strategy_loss, label="Strategy", marker="o")
     plt.title("Loss per round")
     plt.ylabel("DiceLoss")
     plt.xlabel("round")
@@ -16,14 +24,41 @@ def plot_loss(
 
 
 def plot_score(
-    iou_train_logs, iou_val_logs, iou_test_logs, save_path="./output/score.png"
+    full_result, random_result, strategy_result, save_path="./output/score.png"
 ):
-    plt.plot(iou_train_logs, label="train_mIoU", marker="*")
-    plt.plot(iou_val_logs, label="val_mIoU", marker="*")
-    plt.plot(iou_test_logs, label="test_mIoU", marker="*")
+
+    random_score = [log["iou_score"] for log in random_result]
+    strategy_score = [log["iou_score"] for log in strategy_result]
+    full_score = [full_result["iou_score"]] * len(random_score)
+
+    plt.plot(full_score, label="Supervised", linestyle="dashed")
+    plt.plot(random_score, label="Random", marker="o")
+    plt.plot(strategy_score, label="Strategy", marker="o")
     plt.title("Score per round")
     plt.ylabel("mean IoU")
     plt.xlabel("round")
     plt.legend(), plt.grid()
     plt.savefig(save_path)
     # plt.show()
+
+
+def save_json(data: Any, save_path: str) -> None:
+    """Save data to json format.
+    Args:
+        data (Any): The data to store.
+        save_path (str): Path to save.
+    """
+    with open(save_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
+
+def load_json(path: str) -> List[Any]:
+    """Read json data
+    Args:
+        path (str): Path of file to read.
+    Returns:
+        List[Any]: FTSE entity data.
+    """
+    with open(path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return data
