e014197b5eadf77a0bb92343fb699b2914b95a29
BrambleXu <liangxu006@gmail.com>
2022-10-21 16:30:42 +0900

    style: add type hint and docstring

--- segal/strategies/random_sampling.py
+++ segal/strategies/random_sampling.py
@@ -1,21 +1,51 @@
+from typing import List
+
 import numpy as np
+from torch.utils.data import Dataset
 
 from .strategy import Strategy
 
 
 class RandomSampling(Strategy):
+    """Random Sampling Class.
+
+    Args:
+        pool_images (List[str]): List of pool image paths.
+        pool_labels (List[str]): List of pool label paths.
+        val_images (List[str]): List of validation image paths.
+        val_labels (List[str]): List of validation label paths.
+        test_images (List[str]): List of test image paths.
+        test_labels (List[str]): List of test label paths.
+        idxs_lb (List[bool]): List of bool type to record labeled data.
+        model_params (dict): Model parameters.
+                            e.g. model_params = {
+                                    "MODEL_NAME": MODEL_NAME,
+                                    "ENCODER": ENCODER,
+                                    "ENCODER_WEIGHTS": ENCODER_WEIGHTS,
+                                    "NUM_CLASSES": NUM_CLASSES,
+                                }
+        dataset (Dataset): Dataset class.
+        dataset_params (dict): Dataset parameters.
+                            e.g.     dataset_params = {
+                                            "training_augmentation": get_training_augmentation(),
+                                            "validation_augmentation": get_validation_augmentation(),
+                                            "preprocessing": get_preprocessing(preprocessing_fn),
+                                            "classes": CamvidDataset.CLASSES,
+                                        }
+    """
+
     def __init__(
         self,
-        pool_images,
-        pool_labels,
-        val_images,
-        val_labels,
-        test_images,
-        test_labels,
-        idxs_lb,
-        model,
-        dataset,
-        dataset_params,
+        pool_images: List[str],
+        pool_labels: List[str],
+        val_images: List[str],
+        val_labels: List[str],
+        test_images: List[str],
+        test_labels: List[str],
+        idxs_lb: List[bool],
+        model_params: dict,
+        dataset: Dataset,
+        dataset_params: dict,
     ):
         super(RandomSampling, self).__init__(
             pool_images,
@@ -25,12 +55,20 @@ class RandomSampling(Strategy):
             test_images,
             test_labels,
             idxs_lb,
-            model,
+            model_params,
             dataset,
             dataset_params,
         )
 
-    def query(self, n):
+    def query(self, n: int) -> List[int]:
+        """Query data.
+
+        Args:
+            n (int): Number of data to query.
+
+        Returns:
+            List[int]: Indices of queried data.
+        """
         idxs_unlabeled = np.arange(self.n_pool)[
             ~self.idxs_lb
         ]  # reserve the index of unlabeled data
--- segal/utils.py
+++ segal/utils.py
@@ -5,8 +5,19 @@ import matplotlib.pyplot as plt
 
 
 def plot_loss(
-    full_result, random_result, strategy_result, save_path="./output/loss.png"
-):
+    full_result: List[dict],
+    random_result: List[dict],
+    strategy_result: List[dict],
+    save_path: str = "./output/loss.png",
+) -> None:
+    """Plot loss graph.
+
+    Args:
+        full_result (List[dict]): Supervised result.
+        random_result (List[dict]): Random result.
+        strategy_result (List[dict]): Strategy result.
+        save_path (str, optional): Path to save. Defaults to "./output/loss.png".
+    """
 
     random_loss = [log["dice_loss"] for log in random_result]
     strategy_loss = [log["dice_loss"] for log in strategy_result]
@@ -18,15 +29,25 @@ def plot_loss(
     plt.title("Loss per round")
     plt.ylabel("DiceLoss")
     plt.xlabel("round")
-    plt.legend(), plt.grid()
+    plt.legend()
+    plt.grid()
     plt.savefig(save_path)
-    # plt.show()
 
 
 def plot_score(
-    full_result, random_result, strategy_result, save_path="./output/score.png"
-):
+    full_result: List[dict],
+    random_result: List[dict],
+    strategy_result: List[dict],
+    save_path: str = "./output/score.png",
+) -> None:
+    """Plot score graph.
 
+    Args:
+        full_result (List[dict]): Supervised result.
+        random_result (List[dict]): Random result.
+        strategy_result (List[dict]): Strategy result.
+        save_path (str, optional): Path to save. Defaults to "./output/loss.png".
+    """
     random_score = [log["iou_score"] for log in random_result]
     strategy_score = [log["iou_score"] for log in strategy_result]
     full_score = [full_result["iou_score"]] * len(random_score)
@@ -37,9 +58,9 @@ def plot_score(
     plt.title("Score per round")
     plt.ylabel("mean IoU")
     plt.xlabel("round")
-    plt.legend(), plt.grid()
+    plt.legend()
+    plt.grid()
     plt.savefig(save_path)
-    # plt.show()
 
 
 def save_json(data: Any, save_path: str) -> None:
